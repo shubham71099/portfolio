8bec5d2f7f6eed88a939901718bc2056fad67010
“shubham71099” <shubham71099@gmail.com>
2023-08-13 19:27:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 200);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
+
+const sr = ScrollReveal({
+  distance: "40px",
+  duration: 2050,
+  delay: 200,
+  reset: true,
+});
+
+sr.reveal(".hero-text", { origin: "top" });
+sr.reveal(".about-img, .service-item, .about-text ", {
+  origin: "bottom",
+});
+sr.reveal(".about-text h2, .text-center, .right-contact h2", {
+  origin: "top",
+});
+sr.reveal(".left-contact", { origin: "left" });
+sr.reveal(".right-contact", { origin: "right" });
+sr.reveal(".end-section", { origin: "top" });
+
+async function sendEmail() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mobile = document.getElementById("mobile").value;
+  const message = document.getElementById("message").value;
+  const contactForm = document.getElementById("contact-form");
+  const submitBtn = document.getElementById("submit-btn");
+
+  const endpoint =
+    "https://adventurous-sunglasses-frog.cyclic.app/api/email/send";
+
+  const emailMessage = `<b>Name:</b> ${name}<br/><b>Email:</b> ${email}<br/><b>Mobile:</b> ${mobile}<br/><b>Message:</b> ${message}`;
+
+  const body = {
+    emailBody: emailMessage,
+    emailSubject: `New message on portfolio from ${name}`,
+    reciever: "shubham71099@gmail.com",
+    apiKey: "jF45&^dfUE34%fjg",
+  };
+
+  try {
+    submitBtn.innerHTML = "Sending";
+    submitBtn.classList.add("loading");
+
+    const res = await axios.post(endpoint, body, {
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    });
+
+    submitBtn.classList.remove("loading");
+    submitBtn.innerHTML = "Send";
+    contactForm.reset();
+    alert("Email sent successfully! Please wait for response.");
+  } catch (error) {
+    submitBtn.classList.remove("loading");
+    submitBtn.innerHTML = "Send";
+    alert("Something went wrong.");
+  }
+}
