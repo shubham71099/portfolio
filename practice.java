e53dfa26aec5a664af2af8a4d2fbe9c7face4a71
“shubham71099” <shubham71099@gmail.com>
2023-12-19 18:06:07 +0530

    some text file updated

--- practice.java
+++ practice.java
@@ -1,13 +1,20 @@
-//apack/A.java
+//dpack ke under apack, bpack, cpack folder banana hai. file location given below
+
+//dpack/apack/A.java
 package apack;
 
 public class A {
     protected int protectedVar = 10;
     private int privateVar = 20;
     public int publicVar = 30;
+    protected int getProtectedVar() 
+    {
+        return protectedVar;
+    }
+
 }
 
-//bpack/B.java
+//dpack/bpack/B.java
 package bpack;
 import apack.A;
 
@@ -19,27 +26,30 @@ public class B extends A {
     }
 }
 
-//cpack/C.java
+//dpack/cpack/C.java
 package cpack;
 import apack.A;
 
-public class C {
-    public void display() {
+public class C extends A 
+{
+    public void display() 
+    {
         System.out.println("Class C - Accessing variables of class A:");
-        A objA = new A();
-        System.out.println("Protected Variable: " + objA.protectedVar);
-        System.out.println("Public Variable: " + objA.publicVar);
+        System.out.println("Protected Variable: " + getProtectedVar());
+        System.out.println("Public Variable: " + publicVar);
     }
 }
+
 
 //dpack/ProtectedDemo.java
 package dpack;
 import bpack.B;
 import cpack.C;
 
-public class ProtectedDemo {
+public class ProtectedDemo 
+{
     public static void main(String[] args) 
-		{
+	{
         B objB = new B();
         objB.display();
 
@@ -47,3 +57,18 @@ public class ProtectedDemo {
         objC.display();
     }
 }
+
+
+/*
+D:\java\dpack>  javac .\apack\A.java
+D:\java\dpack>  javac .\bpack\B.java
+D:\java\dpack>  javac .\cpack\C.java
+D:\java\dpack>  java ProtectedDemo.java
+
+Class B - Accessing variables of class A:
+Protected Variable: 10
+Public Variable: 30
+Class C - Accessing variables of class A:
+Protected Variable: 10
+Public Variable: 30
+ */
