955831461122949f8dfce01d81c45638d408f94d
“shubham71099” <shubham71099@gmail.com>
2024-08-11 21:55:57 +0530

    Updated email error text

--- script.js
+++ script.js
@@ -145,12 +145,12 @@ async function sendEmail() {
     submitBtn.classList.remove("loading");
     submitBtn.innerHTML = "Send";
     contactForm.reset();
-    alert("Email sent successfully!  I will contact you shortly.");
+    alert("Message sent successfully !  I will contact you shortly.");
   } catch (error) {
     submitBtn.classList.remove("loading");
     submitBtn.innerHTML = "Send";
     contactForm.reset();
-    alert("Email sent successfully!  I will contact you shortly !!");
+    alert("Something went wrong !!");
   }
 }
 
